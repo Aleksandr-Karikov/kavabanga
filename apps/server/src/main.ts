48ed72f54a6069033c694e705435e7a9fcf17652
Aleksandr-Karikov <karikov.aleksandr2803@gmail.com>
2025-07-28 12:12:12 +0700

    chore(server): - drop express packages - turn on logger at fastify adapter

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -3,7 +3,12 @@ import { AppModule } from "./app.module.js";
 import { FastifyAdapter } from "@nestjs/platform-fastify";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, new FastifyAdapter());
+  const app = await NestFactory.create(
+    AppModule,
+    new FastifyAdapter({
+      logger: true,
+    })
+  );
   await app.listen(3010);
 }
 void bootstrap();
